054d772d7c47e320daeb9e32406eb38763f1eed2
Nick Lammer <nila7826@colorado.edu>
2018-03-21 14:33:10 -0600

    First commit, added src/ directory

---
+++ src/config.py
@@ -0,0 +1,3 @@
+BAMS1 = ['/full/path/to/bam1/rep1.bam','/full/path/to/bam1/rep2.bam']
+BAMS2 = ['/full/path/to/bam2/rep1.bam','/full/path/to/bam2/rep2.bam']
+BEDS = ['/full/path/to/bed1/rep1.bed','/full/path/to/bed1/rep2.bed']
---
+++ src/main.py
@@ -0,0 +1,29 @@
+import os
+import some_script
+import config
+
+def run():
+	print("hello world")
+	some_script.execute()
+
+def parse_tab_delimited_file(file1):
+	with open(file1) as F:
+		for line in F:
+			line = line.strip('\n').split('\t')
+			#line will be a list of the columns of your file
+			chrom,start,stop = line
+
+#Takes as input a list of bed files (full path)
+def bedtools_intersect(BEDS):
+	command = "bedtools intersect -a " + BEDS[0] + " -b " + ' '.join(BEDS[1:])
+	print(command)
+	#os.system("bedtools intersect -a " + BEDS[0] + " -b " + ' '.join(BEDS[1:])
+
+if __name__ == "__main__":
+	print("hello different world")
+	run()
+	os.system("ls")
+	print(config.BEDS)
+	a = "hello"
+	print(a+" world2")
+	bedtools_intersect(config.BEDS)
