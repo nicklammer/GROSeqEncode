d05e2272703f877ea6573047fd01a8aede5b8e31
Nick Lammer <nila7826@colorado.edu>
2018-04-06 11:08:55 -0600

    Finalized changes before test run with 0 min time point

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
-BAMS1 = ['/scratch/Users/nila7826/GROSeq/JR_rep1/__','/scratch/Users/nila7826/GROSeq/JR_rep2/__']
-BAMS2 = ['/scratch/Users/nila7826/GROSeq/JR_rep1/__','/scratch/Users/nila7826/GROSeq/JR_rep2/__']
+BAMS1 = ['/scratch/Users/nila7826/GROSeq/JR_rep1/J12_trimmed.flip.fastq.bowtie2.sorted.bam','/scratch/Users/nila7826/GROSeq/JR_rep2/J1DO1_AGTCAA_S1_L007and8_R1_001_trimmed.flip.fastq.bowtie2.sorted.bam']
+BAMS2 = ['/scratch/Users/nila7826/GROSeq/JR_rep1/J22_trimmed.flip.fastq.bowtie2.sorted.bam','/scratch/Users/nila7826/GROSeq/JR_rep2/J2CO1_AGTTCC_S2_L007and8_R1_001_trimmed.flip.fastq.bowtie2.sorted.bam']
 BEDS = ['/scratch/Users/nila7826/ENCODE/ENCFF001UEM.bed','/scratch/Users/nila7826/ENCODE/ENCFF001UEN.bed']
+outdir = '../temp_files/'
--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-import some_script
 import config
 import intersect
 import HTSeq_script
@@ -18,41 +17,20 @@ def run():
     figuredir = parent_dir(homedir)+'/figures/'
     chipfile = filedir + 'rep_intersect.bed'
 
+	print("HTSeq test run using 0 min time point")
+
 	BAMS1 = config.BAMS1
 	BAMS2 = config.BAMS2
 	BEDS = config.BEDS
 	intersect.bedtools_intersect(BEDS,filedir)
 
-	htseq_results = HTSeq_script.run(chipfile,BAMS1,BAMS2)
+	HTSeq_results = HTSeq_script.run(chipfile,BAMS1,BAMS2)
+	print(HTSeq_results)
 
-	#plot.run(htseq_results,figuredir)
-
-
-
-	print("hello world")
-	some_script.execute()
+	#plot.run(HTSeq_results,figuredir)
+	
 
 
-def parse_tab_delimited_file(file1):
-	with open(file1) as F:
-		for line in F:
-			line = line.strip('\n').split('\t')
-			#line will be a list of the columns of your file
-			chrom,start,stop = line
-
-#Takes as input a list of bed files (full path)
-def bedtools_intersect(BEDS,outdir):
-	command = "bedtools intersect -a " + BEDS[0] + " -b " + ' '.join(BEDS[1:])
-	print(command)
-	outfile = outdir+'rep_intersect.bed'
-	#os.system("bedtools intersect -a " + BEDS[0] + " -b " + ' '.join(BEDS[1:] + " > " + outfile)
 
 if __name__ == "__main__":
-	print("hello different world")
 	run()
-	os.system("ls")
-	print(config.BEDS)
-	a = "hello"
-	print(a+" world2")
-	outdir = '../temp_files/'
-	bedtools_intersect(config.BEDS,outdir)
