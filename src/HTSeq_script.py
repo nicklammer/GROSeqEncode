26ade07e27b26ce956adcde770c0d3568def06a4
Nick Lammer <nila7826@colorado.edu>
2018-04-10 18:35:05 -0600

    added normalization for length of region

--- src/HTSeq_script.py
+++ src/HTSeq_script.py
@@ -1,4 +1,5 @@
 #script for counting reads from sorted BAMs in intersected BEDs
+#
 import HTSeq
 import numpy
 
@@ -7,8 +8,7 @@ def run(BED,BAMS1,BAMS2):
 	sortedbamfile1rep2 = HTSeq.BAM_Reader(BAMS1[1])
 	sortedbamfile2rep1 = HTSeq.BAM_Reader(BAMS2[0])
 	sortedbamfile2rep2 = HTSeq.BAM_Reader(BAMS2[1])
-	# bedfile = HTSeq.BED_Reader(BED) #this will probably work. If not, i need to parse it out myself
-
+	
 	bedfile = list()
 	with open(BED) as F:
 		for line in F:
@@ -16,34 +16,39 @@ def run(BED,BAMS1,BAMS2):
 			chrom,start,stop = line[:3]
 			bedfile.append(HTSeq.GenomicInterval(chrom,int(start),int(stop),'.'))
 
-
-	print bedfile[:10]
 	
 	counts1rep1 = list()
-	# counts1rep1 = numpy.zeros(len(bedfile))
 	for region in bedfile:
-		# region = bedfile[i]
 		counts1rep1.append(0.0)
+		length = region.length
 		for almnt in sortedbamfile1rep1[region]:
 			counts1rep1[-1] += 1.0
+		counts1rep1[-1] /= length
+
 
 	counts1rep2 = list()
 	for region in bedfile:
 		counts1rep2.append(0.0)
+		length = region.length
 		for almnt in sortedbamfile1rep2[region]:
 			counts1rep2[-1] += 1.0
+		counts1rep2[-1] /= length
 
 	counts2rep1 = list()
 	for region in bedfile:
 		counts2rep1.append(0.0)
+		length = region.length
 		for almnt in sortedbamfile2rep1[region]:
 			counts2rep1[-1] += 1.0
+		counts2rep1[-1] /= length
 
 	counts2rep2 = list()
 	for region in bedfile:
 		counts2rep2.append(0.0)
+		length = region.length
 		for almnt in sortedbamfile2rep2[region]:
 			counts2rep2[-1] += 1.0
+		counts2rep2[-1] /= length
 
 	counts1avg = [(x+y)/2.0 for x,y in zip(counts1rep1,counts1rep2)]
 	counts2avg = [(x+y)/2.0 for x,y in zip(counts2rep1,counts2rep2)]
